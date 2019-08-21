34f785a04955b6d82825e8f870e4f3aed35b838b
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2019-08-21 16:57:48 +0200

    Use template variables instead of node references

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,4 +3,10 @@ template "/etc/cloud/cloud.cfg" do
   owner "root"
   group "root"
   mode 0644
+  
+  variables(
+    disable_root:      node['cloud']['disable_root'],
+    preserve_hostname: node['cloud']['preserve_hostname'],
+    manage_etc_hosts:  node['cloud']['manage_etc_hosts']
+  )
 end
